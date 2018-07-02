c37ddf1ceb826593feeb714b7dcc92a6967fc4c3
Jinhua Wang <jinhuawang@Jinhuas-MacBook-Air.local>
2018-07-02 17:22:18 +0100

    add multi cluster support

--- pca_cedar.sh
+++ pca_cedar.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --time=12:00:00
-#SBATCH --nodes=100
+#SBATCH --nodes=3
 #SBATCH --mail-user=jw983@jbs.cam.ac.uk
 #SBATCH --mail-type=ALL
 #SBATCH --mem=128G
 module load python27-mpi4py/2.0.0
 module load miniconda2
-srun python pca.py
+mpirun python pca.py
