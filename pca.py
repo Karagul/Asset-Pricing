61bbaccf3858eb7e6d359ec66d1c98b250c96b73
Jinhua Wang <jinhuawang@Jinhuas-MacBook-Air.local>
2018-07-05 18:07:45 +0100

    dropped the index from tail

--- pca.py
+++ pca.py
@@ -85,7 +85,7 @@ def pca_analysis(name):
 
 	#we smooth the quotes by second first, to make sure that when we use increment in unit of second, the noise of quotes will not comeinto play
 	df['TIME_S']=df.TIME_M.map(lambda t:t.strftime('%Y%m%d %H:%M:%S'))
-	df=df.groupby(['SYM_ROOT','DATE','TIME_S']).tail(1).reset_index() #only keep the last observation per interval. forward fill if the value is missing	
+	df=df.groupby(['SYM_ROOT','DATE','TIME_S']).tail(1).reset_index(drop=True) #only keep the last observation per interval. forward fill if the value is missing	
 
 	#calculate the mid price 
 	df['MIDPRICE']=(df['BEST_BID']+df['BEST_ASK'])/2
@@ -101,7 +101,7 @@ def pca_analysis(name):
 	df['diff']=df['TIME_M']-df['genesis'] #get the time difference from the beginning
 	df['diff_sec']=df['diff'].dt.seconds 
 	df['increment']=np.floor(df['diff_sec']/CONST_INTERVAL).astype(int) #the time incremental in CONST_INTERVAL
-	df=df.groupby(['SYM_ROOT','DATE','increment']).tail(1).reset_index() #only keep the last observation per interval. forward fill if the value is missing
+	df=df.groupby(['SYM_ROOT','DATE','increment']).tail(1).reset_index(drop=True) #only keep the last observation per interval. forward fill if the value is missing
 
 	#now expand any gaps in between
 	df=df.groupby(['SYM_ROOT','DATE']).apply(expand_gap)
