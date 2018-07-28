57e7bdede5d4510c5f24d2ccb0867556bee2b6f3
Jinhua Wang <sunpotter@msn.cn>
2018-07-28 10:53:34 +0200

    changed shell script

---
+++ capm_cedar.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --time=3:00:00
+#SBATCH --ntasks=2
+#SBATCH --mail-user=jw983@jbs.cam.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH --mem-per-cpu=20G
+module load python27-mpi4py/2.0.0
+module load miniconda2
+mpirun python capm.py
